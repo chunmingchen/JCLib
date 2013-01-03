4f2e94d59d020329c483cdccc01dc54315b87d6e
migichen <migichen@gmail.com>
2013-01-03 14:52:20 -0500

    .

--- common.h
+++ common.h
@@ -5,7 +5,7 @@ inline void println( const char* format, ... )
 {
     va_list args;
     va_start( args, format );
-    printf( format, args );
+    vprintf( format, args );
     printf("\n");
     va_end( args );
 }
