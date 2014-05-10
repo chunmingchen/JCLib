61fc88163c9ddb1ffc07da5b5ff96413d833ceef
jimmy on yoshi <migichen@gmail.com>
2014-05-09 23:38:32 -0400

    macros

--- macros.h
+++ macros.h
@@ -18,6 +18,18 @@ inline void println( const char* format, ... )
     va_end( args );
 }
 
+inline std::string strprintf( const char * format, ... )
+{
+    va_list args;
+    char *s = new char[strlen(format)+1024];
+    va_start( args, format );
+    vsprintf( s, format, args );
+    std::string str = s;
+    delete[] s;
+    va_end( args );
+    return str;
+}
+
 template<typename T>
 void print_vec( const std::vector<T> &v )
 {
@@ -34,8 +46,20 @@ void print_array( T *v, int n )
 	std::cout << std::endl;
 }
 
+// source: http://stackoverflow.com/questions/1577475/c-sorting-and-keeping-track-of-indexes
+template <class InputIterator>
+std::vector<size_t> sort_indexes(InputIterator begin, InputIterator end) {
 
+  // initialize original index locations
+  std::vector<size_t> idx(end-begin);
+  for (size_t i = 0; i != idx.size(); ++i) idx[i] = i;
 
+  // sort indexes based on comparing values in v
+  sort(idx.begin(), idx.end(),
+       [&begin](size_t i1, size_t i2) {return *(begin+i1) < *(begin+i2);});
+
+  return idx;
+}
 
 }; // namespace jclib
 
